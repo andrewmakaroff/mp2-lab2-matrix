cdc568e19485a091734823b7103fdc3cfc4d6c9c
andrewmakaroff <73525557+andrewmakaroff@users.noreply.github.com>
2021-12-09 01:50:04 +0300

    Add files via upload

--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -31,12 +31,16 @@ TEST(TVector, can_create_copied_vector)
 
 TEST(TVector, copied_vector_is_equal_to_source_one)
 {
-  ADD_FAILURE();
+	TVector<int> source(5);
+	TVector<int> copied(source);
+	ASSERT_EQ(source, copied);
 }
 
 TEST(TVector, copied_vector_has_its_own_memory)
 {
-  ADD_FAILURE();
+	TVector<int> source(3);
+	TVector<int> copied = source;
+	EXPECT_NE(&source[0], &copied[0]);
 }
 
 TEST(TVector, can_get_size)
@@ -63,91 +67,155 @@ TEST(TVector, can_set_and_get_element)
 
 TEST(TVector, throws_when_set_element_with_negative_index)
 {
-  ADD_FAILURE();
+	TVector<int> vec(3);
+	ASSERT_ANY_THROW(vec[-1] = 10);
 }
 
 TEST(TVector, throws_when_set_element_with_too_large_index)
 {
-  ADD_FAILURE();
+	TVector<int> vec(3);
+	ASSERT_ANY_THROW(vec[4] = 10);
 }
 
 TEST(TVector, can_assign_vector_to_itself)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(3);
+	vec_1[0] = 10;
+	vec_2 = vec_1;
+	vec_1 = vec_1;
+	EXPECT_EQ(vec_2, vec_1);
 }
 
 TEST(TVector, can_assign_vectors_of_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(3);
+	vec_1[0] = 10;
+	vec_2 = vec_1;
+	EXPECT_EQ(vec_2, vec_1);
 }
 
 TEST(TVector, assign_operator_change_vector_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(2);
+	vec_1[0] = 10;
+	vec_2 = vec_1;
+	EXPECT_EQ(3, vec_2.GetSize());
 }
 
 TEST(TVector, can_assign_vectors_of_different_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(2);
+	vec_1[0] = 10;
+	vec_2 = vec_1;
+	EXPECT_EQ(vec_2, vec_1);
 }
 
 TEST(TVector, compare_equal_vectors_return_true)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(vec_1);
+	EXPECT_EQ(true, vec_1 == vec_2);
 }
 
 TEST(TVector, compare_vector_with_itself_return_true)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	EXPECT_EQ(true, vec_1 == vec_1);
 }
 
 TEST(TVector, vectors_with_different_size_are_not_equal)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(2);
+	EXPECT_EQ(false, vec_1 == vec_2);
 }
 
 TEST(TVector, can_add_scalar_to_vector)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(3);
+	vec_2 = vec_2 + 1;
+	for (int i = 0; i < vec_1.GetSize(); ++i)
+		vec_1[i] += 1;
+	EXPECT_EQ(vec_1, vec_2);
 }
 
 TEST(TVector, can_subtract_scalar_from_vector)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(3);
+	vec_2 = vec_2 - 1;
+	for (int i = 0; i < vec_1.GetSize(); ++i)
+		vec_1[i] -= 1;
+	EXPECT_EQ(vec_1, vec_2);
 }
 
 TEST(TVector, can_multiply_scalar_by_vector)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(3);
+	vec_2 = vec_2 * 2;
+	for (int i = 0; i < vec_1.GetSize(); ++i)
+		vec_1[i] *= 2;
+	EXPECT_EQ(vec_1, vec_2);
 }
 
 TEST(TVector, can_add_vectors_with_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(3);
+	TVector<int> result(3);
+	vec_1 = vec_1 + 5;
+	result = result + 5;
+	EXPECT_EQ(result, vec_1 + vec_2);
 }
 
 TEST(TVector, cant_add_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(4);
+	ASSERT_ANY_THROW(vec_1 + vec_2);
 }
 
 TEST(TVector, can_subtract_vectors_with_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(3);
+	TVector<int> result(3);
+	vec_1 = vec_1 - 5;
+	result = result + 5;
+	EXPECT_EQ(vec_1, vec_2 - result);
 }
 
 TEST(TVector, cant_subtract_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(4);
+	ASSERT_ANY_THROW(vec_1 - vec_2);
 }
 
 TEST(TVector, can_multiply_vectors_with_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(3);
+	vec_1 = vec_1 + 1;
+	vec_2 = vec_2 + 1;
+	EXPECT_EQ(3, vec_1 * vec_2);
 }
 
 TEST(TVector, cant_multiply_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> vec_1(3);
+	TVector<int> vec_2(4);
+	ASSERT_ANY_THROW(vec_1 * vec_2);
+}
+
+TEST(TVector, pause) 
+{
+	system("pause");
 }
 
